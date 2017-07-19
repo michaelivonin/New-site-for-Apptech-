252a499f3336424092a54b490afa9d72ae127470
Michael Ivonin <Michael@drummer.ru>
2017-07-19 23:58:21 +0600

    Greeting refactor

--- app/js/main.js
+++ app/js/main.js
@@ -3,11 +3,29 @@
  */
 
 $(function() {
+  // Show/hide menu
   $('.header__button-menu').click(function() {
     $('.window-menu').show();
   });
 
   $('.column-menu__button-close').click(function() {
     $('.window-menu').hide();
+  });
+
+  $('.column-menu__link').click(function() {
+    setTimeout(function() {
+      $('.window-menu').hide();
+    }, 1000);
+  });
+
+
+  // Smooth scroll
+  $('.column-menu__link, .banner__button-go-down').click(function(e) {
+    e.preventDefault();
+
+    var target = $(this.hash);
+    $('html, body').stop().animate({
+      'scrollTop': target.offset().top
+    }, 1000);
   });
 });
